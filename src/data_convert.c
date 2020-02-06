e8a36e4dcfce562459eaa54823785f7c0c7d8502
Benjamin Kim <benkim96@gmail.com>
2020-02-05 20:57:32 -0800

    finish shift_tetr

--- src/data_convert.c
+++ src/data_convert.c
@@ -1,21 +1,65 @@
+#include <stdio.h>
+
 void		convert_tetr(char **tetr, char c)
 {
 	int row_index;
 	int col_index;
 
-	row_index = 0;
-	col_index = 0;
-	while (row_index < 5)
+	row_index = -1;
+	while (++row_index < 5)
 	{
-		col_index = 0;
-		while (col_index < 5)
+		col_index = -1;
+		while (++col_index < 5)
 		{
 			if (tetr[row_index][col_index] == '#')
-			{
 				tetr[row_index][col_index] = c;
-			}
-			col_index++;
 		}
-		row_index++;
+	}
+}
+
+int			find_first_index(char **tetr, int mode)
+{
+	int row_index;
+	int col_index;
+
+	row_index = -1;
+	while (++row_index < 5)
+	{
+		col_index = -1;
+		while (++col_index < 5)
+		{
+			if (tetr[row_index][col_index] != '.')
+			{
+				if (mode == 0)
+					return (row_index);
+				else
+					return (col_index);
+			}
+		}
+	}
+	return (row_index);
+}
+
+void		shift_tetr(char **tetr)
+{
+	int row_index;
+	int col_index;
+	int offset_x;
+	int offset_y;
+
+	row_index = -1;
+	offset_x = find_first_index(tetr, 0);
+	offset_y = find_first_index(tetr, 1);
+	while (++row_index < 4)
+	{
+		col_index = -1;
+		while (++col_index < 4)
+		{
+			if (tetr[row_index][col_index] != '.')
+			{
+				tetr[row_index - offset_x][col_index - offset_y] = tetr[row_index][col_index];
+				tetr[row_index][col_index] = '.';
+			}
+		}
 	}
 }
